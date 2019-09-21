8c6d9606c4ba7289eeb0ab12312a085082d90154
Vinh Ta <Vinh Ta>
2019-09-21 18:43:12 +0100

    created empty html grid

---
+++ src/resources/js/main.js
@@ -0,0 +1,15 @@
+var grid = createGrid(40, 40)
+document.body.appendChild(grid)
+
+function createGrid(n_rows, n_columns){
+    var grid = document.createElement('table')
+    grid.className = 'grid'
+    for (var j=0; j<n_rows; ++j){
+        var tr = grid.appendChild(document.createElement('tr'))
+        for (var i=0; i<n_columns; ++i){
+            var td = grid.appendChild(document.createElement('td'))
+
+        }
+    }
+    return grid
+}
